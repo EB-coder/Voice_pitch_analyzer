b895b03b3c26a813ff4b9fd76b535c2a839f35cb
last_coder <73636880+EB-coder@users.noreply.github.com>
2025-04-22 20:05:33 +0600

    Update record_audio.py

--- record_audio.py
+++ record_audio.py
@@ -1,53 +1,7 @@
-# import pyaudio
-# import wave
-
-
-
-# # Параметры записи
-# FORMAT = pyaudio.paInt16      # 16-бит
-# CHANNELS = 1                  # Моно
-# RATE = 44100                 # Частота дискретизации
-# CHUNK = 1024                 # Размер блока
-# RECORD_SECONDS = 10          # Длительность записи
-# OUTPUT_FILENAME = "voice.wav"  # Имя выходного файла
-
-# # Инициализация
-# audio = pyaudio.PyAudio()
-
-# stream = audio.open(format=FORMAT,
-#                     channels=CHANNELS,
-#                     rate=RATE,
-#                     input=True,
-#                     frames_per_buffer=CHUNK)
-
-# print("🎙️ Pls speak...")
-
-# frames = []
-
-# for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-#     data = stream.read(CHUNK)
-#     frames.append(data)
-
-# print("✅ Recording complete.")
-
-# # Остановить и закрыть
-# stream.stop_stream()
-# stream.close()
-# audio.terminate()
-
-# # Сохранить в WAV файл
-# with wave.open(OUTPUT_FILENAME, 'wb') as wf:
-#     wf.setnchannels(CHANNELS)
-#     wf.setsampwidth(audio.get_sample_size(FORMAT))
-#     wf.setframerate(RATE)
-#     wf.writeframes(b''.join(frames))
-
-# print(f"📁 File saved as {OUTPUT_FILENAME}")
-
 import sounddevice as sd
 import numpy as np
 import wave
-import sys  # Добавлено для вывода в реальном времени
+import sys
 
 def record_audio():
     RATE = 44100
@@ -56,7 +10,7 @@ def record_audio():
     RECORD_SECONDS = 10
     OUTPUT_FILENAME = "voice.wav"
 
-    print("🎙️ Recording started...", flush=True)  # flush для Streamlit
+    print("🎙️ Recording started...", flush=True)
     
     try:
         audio = sd.rec(
